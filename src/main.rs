7e2706f539cc11c6bad1a789048a8b7b6239628a
Alexander Nookinov <anookinov@gmail.com>
2021-08-23 00:48:31 +0700

    store closures using generic parameters and the Fn traits

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,77 @@
 use std::thread;
 use std::time::Duration;
 
-fn simulated_expensive_calculation(intensity: u32) -> u32 {
-    println!("calculating slowly...");
-    thread::sleep(Duration::from_secs(2));
-    intensity
+struct Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    calculation: T,
+    value: Option<u32>,
 }
+
+impl<T> Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    fn new(calculation: T) -> Cacher<T> {
+        Cacher {
+            calculation,
+            value: None,
+        }
+    }
+
+    fn value(&mut self, arg: u32) -> u32 {
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v
+            }
+        }
+    }
+}
+
+// fn simulated_expensive_calculation(intensity: u32) -> u32 {
+//     println!("calculating slowly...");
+//     thread::sleep(Duration::from_secs(2));
+//     intensity
+// }
 
 fn generate_workout(intensity: u32, random_number: u32) {
     // closure 2 params i.e. |param1, param2|
-    let expensive_closure = |num| {
+    let mut expensive_result = Cacher::new(|num| {
         println!("calculating slowly...");
         thread::sleep(Duration::from_secs(2));
         num
-    };
+    });
+
+    // let expensive_closure_type_annotate = |num: u32| -> u32 {
+    //     println!("calculating slowly...");
+    //     thread::sleep(Duration::from_secs(2));
+    //     num
+    // };
+
+    // fn  add_one_v1   (x: u32) -> u32 { x + 1 }
+    // let add_one_v2 = |x: u32| -> u32 { x + 1 };
+    // let add_one_v3 = |x|             { x + 1 };
+    // let add_one_v4 = |x|               x + 1  ;
+
+    // let example_closure = |x| x;
+    // let s = example_closure(String::from("hello"));
+    // let n = example_closure(5); // error as closure definition will have one concrete type inferred
 
     if intensity < 25 {
-        println!("Today, do {} pushups!", expensive_closure(intensity));
-        println!("Next, do {} situps!", expensive_closure(intensity));
+        println!("Today, do {} pushups!", expensive_result.value(intensity));
+        println!("Next, do {} situps!", expensive_result.value(intensity));
     } else {
         if random_number == 3 {
             println!("Take a break today! Remember to stay hydrated!");
         } else {
-            println!("Today, run for {} minutes!", expensive_closure(intensity));
+            println!(
+                "Today, run for {} minutes!",
+                expensive_result.value(intensity)
+            );
         }
     }
 }
